a7da2ba6319ffd4ce188d87eda4d375a141d912d
ryotako <kobayashiryota2046@gmail.com>
2017-03-20 20:50:28 +0900

    fix expand_range to ignore string literals

--- functions/expand_range.fish
+++ functions/expand_range.fish
@@ -1,15 +1,22 @@
 function expand_range -d 'expand {X..Y} in arguments with bash\'s brace expansion'
-    set -l line "$argv"
+    set -l input "$argv"
+    set -l buf $input
+    set -l output $input 
 
-    for brace in (string match -ar '\{-?\w+\.\.-?\w+\}' $line)
-        set -l expanded (bash -c "echo $brace")
+    set -l buf (string replace -ar '\\\\[\\\\\'"]' '  ' $buf)
 
-        if test $brace != $expanded
-            set -l csv (string replace -a ' ' ',' $expanded)
+    for pos_len in (string match -anr '\'[^\']*\'|"[^"]*"|\{-?\w+\.\.-?\w+(\.\.-?\w+)?\}' $buf)
+        echo $pos_len | read -l pos len
+        set -l matched (string sub -s $pos -l $len $input)
 
-            set line (string replace $brace "{$csv}" $line)
+        if string match -q '{*}' $matched
+            set -l head (string sub -l $pos $input)
+            set -l body (string replace -a ' ' , (bash -c "echo $matched"))
+            set -l tail (string sub -s (math "$pos+$len-1") $input)
+
+            set output "$head$body$tail"
         end
     end
 
-    echo $line
+    echo $output
 end
