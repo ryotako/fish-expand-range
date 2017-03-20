177f163e39c4a77c49c5637eacd81ad3c20d71b5
ryotako <kobayashiryota2046@gmail.com>
2017-03-20 22:14:04 +0900

    fix expand_range to expand multiple braces

--- functions/expand_range.fish
+++ functions/expand_range.fish
@@ -1,22 +1,29 @@
 function expand_range -d 'expand {X..Y} in arguments with bash\'s brace expansion'
-    set -l input "$argv"
-    set -l buf $input
-    set -l output $input 
 
-    set -l buf (string replace -ar '\\\\[\\\\\'"]' '  ' $buf)
+    set -l match_params (
+        string replace -ar '\\\\[\\\\\'"]' '  ' "$argv" \
+        | string match -anr '\'[^\']*\'|"[^"]*"|\{-?\w+\.\.-?\w+(\.\.-?\w+)?\}'
+    )
 
-    for pos_len in (string match -anr '\'[^\']*\'|"[^"]*"|\{-?\w+\.\.-?\w+(\.\.-?\w+)?\}' $buf)
-        echo $pos_len | read -l pos len
-        set -l matched (string sub -s $pos -l $len $input)
 
-        if string match -q '{*}' $matched
-            set -l head (string sub -l $pos $input)
-            set -l body (string replace -a ' ' , (bash -c "echo $matched"))
-            set -l tail (string sub -s (math "$pos+$len-1") $input)
+    if test (count $match_params) -lt 1
+        echo "$argv"
+    else
+        set -l output $argv
 
-            set output "$head$body$tail"
+        for pos_len in $match_params[-1..1]
+            echo $pos_len | read -l pos len
+            set -l matched (string sub -s $pos -l $len $output)
+
+            if string match -q '{*}' $matched
+                set -l head (string sub -l $pos $output)
+                set -l body (string replace -a ' ' , (bash -c "echo $matched"))
+                set -l tail (string sub -s (math "$pos+$len-1") $output)
+
+                set output "$head$body$tail"
+            end
         end
-    end
 
-    echo $output
+        echo $output
+    end
 end
